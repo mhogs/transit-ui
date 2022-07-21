431a72132dcfbd384b948e2a8d939033cde2ce93
amr dar <amrjmd1@gmail.com>
2022-07-21 18:12:38 +0300

    Card Component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,32 @@
+import React, { memo, ReactNode } from 'react'
+import { Text, View } from 'react-native'
+
+type Props = {
+    children?: ReactNode
+    style?: any
+}
+const Card = ({ children, style }: Props) => {
+
+    return <View
+        style={{
+            shadowColor: "#000",
+            shadowOffset: {
+                width: 1,
+                height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 5,
+            paddingHorizontal: 16,
+            paddingVertical: 10,
+            backgroundColor: '#fff',
+            marginVertical: 5,
+            borderRadius: 8,
+            ...style
+        }}
+    >
+        {children}
+    </View>
+}
+
+export default memo(Card)
